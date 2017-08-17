fa9349f0c67ec49dd551bd9a3a0c3f166d62d9e1
jonesth <tjones2350@gmail.com>
2017-08-17 13:31:59 +0100

    Delete plot1.R

--- plot1.R
+++
@@ -1,9 +0,0 @@
-### Plot 1
-dataFile <- "household_power_consumption.txt" #names the dataset as dataFile
-fullData <- read.table(dataFile, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")  #reads the dataset into a tabel in R
-subSetData <- fullData[fullData$Date %in% c("1/2/2007","2/2/2007"),]  #subsets the fullData set into the two days of interest
-
-gloActPwr <- as.numeric(subSetData$Global_active_power) #converts Global_active_power column to a numeric
-png("plot1.png", width = 480, height = 480) #creates an empty png file
-hist(gloActPwr, xlab = "Global Active Power (kilowatts)", ylab = "Frequency", col = "red", main = "Global Active Power") #creates historgram
-dev.off()
